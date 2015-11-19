762c14a5ff542488c0d3ef345df3520b2f120382
McKay Ririe <mrir2917@740s-pl-123.local>
2015-11-19 07:30:46 -0700

    Added a class

--- src/gui_controller/GUIAppController.java
+++ src/gui_controller/GUIAppController.java
@@ -2,6 +2,16 @@ package gui_controller;
 
 import gui.view.GUIAppFrame;
 
-public class GUIAppController {
-
+public class GUIAppController
+{
+	private GUIAppFrame baseFrame;
+	
+	public GUIAppController()
+	{
+		baseFrame = new GUIAppFrame(this);
+	}
+	public void start()
+	{
+		
+	}
 }
